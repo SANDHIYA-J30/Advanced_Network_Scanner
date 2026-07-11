0fbc317ae1e2a0d4814bbee318f68e82ac5d2bf0
SANDHIYA-J30 <sandhiyajayaraman06@gmail.com>
2026-07-11 16:11:28 +0530

    Configure DATABASE_URL for deployment

--- network_scanner/settings.py
+++ network_scanner/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'network_scanner.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"postgres://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
+        default=config("DATABASE_URL")
     )
 }
 
